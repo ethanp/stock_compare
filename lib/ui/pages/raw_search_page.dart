14726117a664e5bd9bb643a4a5136c8f59274ac9
Ethan Petuchowski <etahnp@gmail.com>
2024-12-08 19:54:12 -0600

    Document more of what the program does.

--- lib/ui/pages/raw_search_page.dart
+++ lib/ui/pages/raw_search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yahoo_finance_data_reader/yahoo_finance_data_reader.dart';
 
+/// This example simply downloads daily data for a const `ticker` (viz. Solana),
+/// then plops the raw response data on the screen.
 class RawSearch extends StatefulWidget {
   const RawSearch();
 
@@ -11,40 +13,25 @@ class RawSearch extends StatefulWidget {
 class _RawSearchState extends State<RawSearch> {
   @override
   Widget build(BuildContext context) {
-    const String ticker = 'SOL-USD';
-    const YahooFinanceDailyReader yahooFinanceDataReader =
-        YahooFinanceDailyReader();
-
-    final Future<Map<String, dynamic>> future =
-        yahooFinanceDataReader.getDailyData(ticker);
-    debugPrint('getting data for $ticker');
-
     return FutureBuilder(
-      future: future,
-      builder:
-          (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.data == null) return const Text('No data');
-          final Map<String, dynamic> historicalData = snapshot.data!;
-          return SingleChildScrollView(child: Text(historicalData.toString()));
-        } else if (snapshot.hasError) {
-          return Text('Error ${snapshot.error}');
-        }
-
-        return const Center(
-          child: SizedBox(
-            height: 50,
-            width: 50,
-            child: CircularProgressIndicator(),
-          ),
-        );
+      future: const YahooFinanceDailyReader().getDailyData('SOL-USD'),
+      builder: (_, snapshot) {
+        if (snapshot.hasError) return Text('Error ${snapshot.error}');
+        if (snapshot.connectionState != ConnectionState.done) return loading();
+        if (snapshot.data == null) return const Text('No data');
+        final Map<String, dynamic> historicalData = snapshot.data!;
+        return SingleChildScrollView(child: Text(historicalData.toString()));
       },
     );
   }
 
-  String generateDescription(DateTime date, Map<String, dynamic> day) {
-    final names = ['open', 'close', 'high', 'low', 'adjclose'];
-    final values = names.map((n) => '$n: ${day[n]}').join('\n');
-    return '$date\n$values';
+  Widget loading() {
+    return const Center(
+      child: SizedBox(
+        height: 50,
+        width: 50,
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
